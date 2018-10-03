9ef89b57d65e9b2f37e3df8642d4f905d785d5b7
Hunter Stutts <stuttskyle@gmail.com>
2018-10-03 16:15:53 -0500

    more polishing:

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+////read and rquire dotenv package
 require("dotenv").config();
 
+//import keys
 const keys = require('./keys.js');
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
